d871b6ca44f92f9c88cac6fb24d061d46536756d
ChuangshiJiang <30435246+ChuangshiJiang@users.noreply.github.com>
2018-05-10 17:14:36 +0800

    共享结构的对象，提高性能

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 
-function renderApp(appState) {
-    renderTitle(appState.title);
-    renderContent(appState.content);
+function renderApp(newAppState,oldAppState = {}) {//防止没有 oldAppState
+    if(newAppState === oldAppState) return;//数据没有变化就不做渲染
+    console.log('render app...');
+    renderTitle(newAppState.title,oldAppState.title);
+    renderContent(newAppState.content,oldAppState.content);
 }
 
-function renderTitle(title) {
+function renderTitle(newTitle,oldTitle = {}) {
+    if(newTitle === oldTitle) return ;//数据没有变化就不做渲染
+    console.log('render title...');
     const titleDOM = document.getElementById('title');
-    titleDOM.innerHTML = title.text;
-    titleDOM.style.color = title.color;
+    titleDOM.innerHTML = newTitle.text;
+    titleDOM.style.color = newTitle.color;
 }
 
-function renderContent(content) {
+function renderContent(newContent,oldContent = {}) {
+    if(newContent === oldContent) return ;//数据没有变化就不做渲染
+    console.log('render content...');
     const contentDOM = document.getElementById('content');
-    contentDOM.innerHTML = content.text;
-    contentDOM.style.color = content.color;
+    contentDOM.innerHTML = newContent.text;
+    contentDOM.style.color = newContent.color;
 }
 
 let appState = {
@@ -27,15 +33,29 @@ let appState = {
     }
 }
 
-function stateChanger(action) {
+function stateChanger(state,action) {
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text;
+            return {
+                //构建新的对象并返回
+                ...state,
+                title:{
+                    ...state.title,
+                    text:action.text
+                }
+            }
             break;
         case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color;
+            return {
+                ...state,
+                title:{
+                    ...state.title,
+                    color:action.color
+                }
+            }
             break;
         default:
+            return state;
             break;
     }
 }
@@ -45,15 +65,19 @@ function createStore(state,stateChanger){
     const subscribe = (listener)=>listeners.push(listener);
     const getState = ()=>state
     const dispatch = (action)=>{
-        stateChanger(action)
+        state = stateChanger(state,action)//覆盖原对象
         listeners.forEach((listener)=>listener())
     }
     return {getState,dispatch,subscribe}
 }
 
 const store = createStore(appState,stateChanger);
-
-store.subscribe(()=>renderApp(store.getState()));
+let oldState = store.getState();//缓存旧的 state
+store.subscribe(()=>{
+    const newState = store.getState();//数据可能变化，获取新的state
+    renderApp(newState,oldState);//把新旧的 oldState传进去渲染
+    oldState = newState;//渲染完成后新的 newState 变成旧的 oldState
+});
 
 
 renderApp(store.getState())//首次渲染页面
@@ -65,7 +89,5 @@ store.dispatch({
 
 store.dispatch({
     type:'UPDATE_TITLE_COLOR',
-    color:'blue'
-})
-
-renderApp(store.getState());
+    color:'#33b500'
+})//修改标题颜色
