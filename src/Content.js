eba5e12f0917cd6397fc5464f6dac119fa39c7fb
ChuangshiJiang <jcsweiyi@outlook.com>
2018-05-13 22:19:01 +0800

    结合 context 和 store

--- src/Content.js
+++ src/Content.js
@@ -5,10 +5,31 @@ import ThemeSwitch from './ThemeSwitch'
 import Header from './Header'
 
 class Content extends Component{
+    static contextTypes={
+        store:PropTypes.object
+    }
+
+    constructor(){
+        super();
+        this.state = {themeColor:''}
+    }
+
+    componentWillMount(){
+        const {store}=this.context;
+        this._updateThemeColor();
+        store.subscribe(()=>this._updateThemeColor());
+    }
+
+    _updateThemeColor(){
+        const {store} = this.context;
+        const state = store.getState();
+        this.setState({themeColor:state.themeColor});
+    }
+
     render(){
         return (
             <div>
-                <p> React.js 小书内容 </p>
+                <p style={{color:this.state.themeColor}}> React.js 小书内容 </p>
                 <ThemeSwitch />
             </div>
         )
