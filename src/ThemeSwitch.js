a37f7918c6f6c4239ed9e1213a0026bc09868f18
ChuangshiJiang <jcsweiyi@outlook.com>
2018-05-15 23:07:32 +0800

    mapDispatchProps

--- src/ThemeSwitch.js
+++ src/ThemeSwitch.js
@@ -1,36 +1,17 @@
 import React,{Component} from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux';
 
 class ThemeSwitch extends Component{
     static contextTypes={
         store:PropTypes.object
     }
 
-    constructor(){
-        super();
-        this.state = {themeColor:''}
-    }
-
-    componentWillMount(){
-        const {store}=this.context;
-        this._updateThemeColor();
-        store.subscribe(()=>this._updateThemeColor());
-
-    }
-
-    _updateThemeColor(){
-        const {store} = this.context;
-        const state = store.getState();
-        this.setState({themeColor:state.themeColor});
-    }
-
     // diapatch action 去改变颜色
     handleSwitchColor(color){
-        const {store}=this.context;
-        store.dispatch({
-            type:'CHANGE_COLOR',
-            themeColor:color
-        });
+        if(this.props.onSwitchColor){
+            this.props.onSwitchColor(color);
+        }
     }
 
     render(){
@@ -48,5 +29,21 @@ class ThemeSwitch extends Component{
         )
     }
 }
+
+const mapStateToProps = (state)=>{
+    return {
+        themeColor:state.themeColor
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        onSwitchColor:(color)=>{
+            dispatch({type:'CHANGE_COLOR',themeColor:color})
+        }
+    }
+}
+
+ThemeSwitch = connect(mapStateToProps,mapDispatchToProps)(ThemeSwitch);
 
 export default ThemeSwitch
