f73ec4671f094ba2d4f146dbc4e862d2dfc2db7b
ChuangshiJiang <30435246+ChuangshiJiang@users.noreply.github.com>
2018-05-17 14:33:09 +0800

    smart and dumb

---
+++ src/components/ThemeSwitch.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+import PropTypes from 'prop-types'
+
+class ThemeSwitch extends Component{
+    static contextTypes={
+        themeColor:PropTypes.string,
+        store:PropTypes.object
+    }
+
+    // diapatch action 去改变颜色
+    handleSwitchColor(color){
+        if(this.props.onSwitchColor){
+            this.props.onSwitchColor(color);
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <button 
+                style={{color:this.props.themeColor}}
+                onClick={this.handleSwitchColor.bind(this,'red')}
+                > Red </button>
+                <button 
+                style={{color:this.props.themeColor}} 
+                onClick={this.handleSwitchColor.bind(this,'blue')}
+                > Blue </button>
+            </div>
+        )
+    }
+}
+
+export default ThemeSwitch
