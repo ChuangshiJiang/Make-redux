5e4dce9a2a2ab99e0c64c2f9df86bc2370a8eace
ChuangshiJiang <jcsweiyi@outlook.com>
2018-06-19 07:29:24 +0800

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,92 +1,67 @@
-/*
-* 渲染函数
-*/
-
-
-function renderApp(newAppState,oldAppState = {}) {
-    if(newAppState === oldAppState) return; //数据没有变化就不渲染
-    console.log("render app...");
-    renderTitle(newAppState.title,oldAppState.title);
-    renderContent(newAppState.content,oldAppState.content);
-}
-
-function renderTitle(newTitle,oldTitle={}) {
-    if(newTitle === oldTitle) return; //数据没有变化就不渲染
-    console.log("render title...");
-    const titleDOM = document.getElementById('title');
-    titleDOM.innerHTML = newTitle.text;
-    titleDOM.style.color = newTitle.color;
-}
-
-function renderContent(newContent,oldContent={}) {
-    if(newContent === oldContent) return; //数据没有变化就不渲染
-    console.log("render content...");
-    const contentDOM = document.getElementById('content');
-    contentDOM.innerHTML = newContent.text;
-    contentDOM.style.color = newContent.color;
-}
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Content from './Content'
+import './index.css'
 
 function createStore(reducer) {
     let state = null;
     const listeners = [];
     const subscribe = (listener) => listeners.push(listener);
     const getState = () => state;
-    const dispatch = (action) => {
+    const dispathch = (action) => {
         state = reducer(state, action);
         listeners.forEach((listener) => listener());
-    };
-    dispatch({});
-    return { getState, dispatch, subscribe };
+    }
+
+    dispathch({});//初始化 state
+    return { getState, dispathch, subscribe };
 }
 
-/*派发函数*/
-function reducer(state, action) {
-    if(!state){
-        return{
-            title: {
-                text: 'React.js 小书',
-                color: 'red'
-            },
-            content: {
-                text: 'React.js 小书内容',
-                color: 'blue'
-            }
+const themeReduecer = (state, action) => {
+    if (!state) {
+        return {
+            themeColor: 'red'
         }
     }
+
     switch (action.type) {
-        case 'UPDATE_TITLE_TEXT':
+        case 'CHANGE_COLOR':
             return {
                 ...state,
-                title:{
-                    ...state.title,
-                    text:action.text
-                }
+                themeColor: action.themeColor
             }
-        case 'UPDATE_TITLE_COLOR':
-        return {
-            ...state,
-            title:{
-                ...state.title,
-                color:action.color
-            }
-        }
         default:
-            return state;//没有修改，返回原来的对象
+            return state;
     }
 }
 
-const store = createStore(reducer);
-let oldState = store.getState();    //缓存久的 state
-store.subscribe(() => {
-    const newState = store.getState();// 数据可能变化，获取新的 state
-    renderApp(newState,oldState);// 把新旧的 state 传进去渲染
-    oldState = newState;// 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
-});
 
-renderApp(store.getState());
+const store = createStore(themeReduecer);
 
-setTimeout(() => {
-    store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本    
-}, 1000);
+class Index extends Component {
+    static childContextTypes = {
+        store:PropTypes.object
+    }
 
-store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
+    getChildContext(){
+        return{
+            store
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Content />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
