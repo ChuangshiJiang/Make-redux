a55d52e2d095bdc7e7ca04d11fa76665afd59923
ChuangshiJiang <30435246+ChuangshiJiang@users.noreply.github.com>
2018-06-21 14:05:31 +0800

    使用真正的 Redux 和 React-redux

--- src/index.js
+++ src/index.js
@@ -3,22 +3,23 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import Header from './Header';
 import Content from './Content';
+import { createStore } from 'redux'
 import './index.css';
 import {Provider} from './react-redux';
 
-function createStore(reducer) {
-    let state = null;
-    const listeners = [];
-    const subscribe = (listener) => listeners.push(listener);
-    const getState = () => state;
-    const dispatch = (action) => {
-        state = reducer(state, action);
-        listeners.forEach((listener) => listener());
-    }
+// function createStore(reducer) {
+//     let state = null;
+//     const listeners = [];
+//     const subscribe = (listener) => listeners.push(listener);
+//     const getState = () => state;
+//     const dispatch = (action) => {
+//         state = reducer(state, action);
+//         listeners.forEach((listener) => listener());
+//     }
 
-    dispatch({});//初始化 state
-    return { getState, dispatch, subscribe };
-}
+//     dispatch({});//初始化 state
+//     return { getState, dispatch, subscribe };
+// }
 
 const themeReduecer = (state, action) => {
     if (!state) {
